0f2600ae02362835f72c99f7523e2a902e83e9fc
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-21 00:21:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your solution here!
-const append ;
-const prepend;
-const removeLast;
+const append      =[] ;
+const prepend     =[] ;
+const removeLast  =[] ;
+const removeFirst =[] ;
 
 
