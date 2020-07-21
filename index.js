5002ba53e8d8f4e4b2e0e12233344ea3a2150f6f
Dhiaa Alshamy <dhodhof@gmail.com>
2020-07-21 00:29:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let prepend     =['Milo', 'Otis', 'Garfield'] ;
 let removeLast  =['Milo', 'Otis', 'Garfield'] ;
 let removeFirst =['Milo', 'Otis', 'Garfield'] ;
 append.push('Odie');
-appind.unshift('Odie');
+prepend.unshift('Odie');
 removeLast.pop();
 removeFirst.shift();
 
